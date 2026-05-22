2afd3a77604108ade65ae401d239b661ef053053
Moziful Haque <mhmoni2310@gmail.com>
2026-05-22 23:48:16 +0600

    Added Trusted Origins

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -20,6 +20,12 @@ const db = client.db("PetBuddy");
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [
+    process.env.BETTER_AUTH_URL,
+    process.env.SERVER_URL,
+    process.env.CLIENT_URL,
+    "http://localhost:3000",
+  ].filter(Boolean),
   emailAndPassword: {
     enabled: true,
   },
